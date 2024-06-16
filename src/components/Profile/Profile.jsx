390b1171f46121e326a8e5885a556410dc820142
Max-IT-ZT <babuskomax@gmail.com>
2024-06-16 13:47:59 +0300

    fix props

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,25 +1,25 @@
 import css from "./Profile.module.css";
-export default function Profile(props) {
+export default function Profile({ name, tag, location, image, stats }) {
   return (
     <div className={css.container}>
       <div className={css.profileCard}>
-        <img className={css.profileImg} src={props.image} alt={props.name} />
-        <p className={css.profileName}>{props.name}</p>
-        <p className={css.profileTag}>@{props.tag}</p>
-        <p className={css.profileLocation}>{props.location}</p>
+        <img className={css.profileImg} src={image} alt={name} />
+        <p className={css.profileName}>{name}</p>
+        <p className={css.profileTag}>@{tag}</p>
+        <p className={css.profileLocation}>{location}</p>
       </div>
       <ul className={css.profileList}>
         <li className={css.profileItem}>
           <span className={css.profileText}>Followers</span>
-          <span className={css.profileNumbers}>{props.stats.followers}</span>
+          <span className={css.profileNumbers}>{stats.followers}</span>
         </li>
         <li className={css.profileItem}>
           <span className={css.profileText}>Views</span>
-          <span className={css.profileNumbers}>{props.stats.views}</span>
+          <span className={css.profileNumbers}>{stats.views}</span>
         </li>
         <li className={css.profileItem}>
           <span className={css.profileText}>Likes</span>
-          <span className={css.profileNumbers}>{props.stats.likes}</span>
+          <span className={css.profileNumbers}>{stats.likes}</span>
         </li>
       </ul>
     </div>
